cbc27782788fbdf18ea189af43b5b065f128c58d
Hari Haran kirsh <smarthari.2810@gmail.com>
2026-04-15 17:17:43 +0530

    Updates

---
+++ _next/static/qiPyu6l3YcAs8X_ypRsDz/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/skape-parallax//_next/:path+",
+        "destination": "/skape-parallax/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
