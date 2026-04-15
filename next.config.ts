6e22d98b1d6b4328cf6ec0ab3a56346918ee8441
Hari Haran kirsh <smarthari.2810@gmail.com>
2026-04-15 17:14:52 +0530

    Update configuration files and add gh-pages dependency for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",   // ✅ VERY IMPORTANT
   images: {
-    formats: ["image/avif", "image/webp"]
-  }
+    unoptimized: true,  // ✅ required for GitHub Pages
+  },
+  basePath: "/skape-parallax",   // ⚠️ replace with your repo name
+  assetPrefix: "/skape-parallax/",
 };
 
 export default nextConfig;
